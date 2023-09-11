d7f39676ffe696e6071802204e1627b9c11f9078
Giorgio Tuccio <Giorgio.Tuccio@pefcu.com>
2023-09-10 20:01:55 -0400

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameAreaComponent } from './components/game-area/game-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameAreaComponent
   ],
   imports: [
     BrowserModule,
